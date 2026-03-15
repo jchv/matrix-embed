8b10f25c5dfc8a36d8de38f16a3c5dcc519244c4
John Chadwick <john@jchw.io>
2026-03-15 09:56:39 -0400

    Default to info logging

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,12 @@ struct SavedSession {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
 
     // Load config from CLI args/files
     let config = Config::load().await?;
